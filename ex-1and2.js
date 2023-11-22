cb67fca2fbf8afabf978333d0c2d1d67630226a5
Mae Okubo <okubo.mae@gmail.com>
2023-11-22 23:33:26 +0900

    Done Assignment Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,25 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let  firstName ="Karter";
+let  lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+console.log(firstName +  lastName + address);
+
+const names = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const total = totalPrice + 1500;
+console.log(names + " totalPrice " + total);
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”;
+console.log(address);
+
+totalPrice =  5000;
+console.log(totalPrice);
+
+total = totalPrice + 2500;
+console.log(total);
+
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,9 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
+celsius = (celsius * 1.8) + 32;
+console.log(celsius);
 // Start coding here;
 
-let fahrenheit;
+let fahrenheit = celsius;
+fahrenheit = (fahrenheit - 32) * 5 / 9;
+console.log(fahrenheit);
